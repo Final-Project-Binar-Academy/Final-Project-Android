1955cf51d976b44ee985662e31a89fec0fb4efd2
Aiffah Kiysa Waafi <aiffahwaafi6@gmail.com>
2022-11-27 21:42:17 +0700

    update layout login register

--- app/src/main/java/com/binar/finalproject14/ui/LoginFragment.kt
+++ app/src/main/java/com/binar/finalproject14/ui/LoginFragment.kt
@@ -1,10 +1,11 @@
-package com.binar.finalproject14
+package com.binar.finalproject14.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.binar.finalproject14.R
 
 class LoginFragment : Fragment() {
 
